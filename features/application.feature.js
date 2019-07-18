ef3977ba8442b66644762ad97cac35becb6dae4a
Max Aubain <max.aubain@gmail.com>
2019-07-18 21:52:53 +0200

    Adds feature tests for when player clicks an RPS button and a message is displayed.

--- features/application.feature.js
+++ features/application.feature.js
@@ -1,6 +1,7 @@
 require('../spec.helper');
 
 context('Your Description of the test scenario', () => {
+  // Browser helpers
   before(async () => {
     await browser.init()
     await browser.visitPage('http://localhost:8080/')
@@ -14,6 +15,7 @@ context('Your Description of the test scenario', () => {
     browser.close();
   });
 
+  // Testing for game labels
   it('renders the correct page title', async () => {
     expect(await browser.page.title()).to.eql('RPS');
   });
@@ -22,4 +24,35 @@ context('Your Description of the test scenario', () => {
     let content = await browser.getContent("div[class='title']")
     expect(content).to.contain('Scissors!');
   });
+
+  it('renders user player tab', async () => {
+    let content = await browser.getContent("div[id='player1']")
+    expect(content).to.contain('Player 1');
+  });
+
+  it('renders computer player tab', async () => {
+    let content = await browser.getContent("div[id='player2']")
+    expect(content).to.contain('Computer');
+  });
+
+  // Testing for user player buttons
+  it('click on Rock button', async () => {
+    await browser.clickOnButton("button[id='button-player-rock']")
+    let content = await browser.getContent("div[class='inner-left']")
+    expect(content).to.contain('You chose rock.');
+  });
+
+  it('click on Paper button', async () => {
+    await browser.clickOnButton("button[id='button-player-paper']")
+    let content = await browser.getContent("div[class='inner-left']")
+    expect(content).to.contain('You chose paper.');
+  });
+
+  it('click on Scissors button', async () => {
+    await browser.clickOnButton("button[id='button-player-scissors']")
+    let content = await browser.getContent("div[class='inner-left']")
+    expect(content).to.contain('You chose scissors.');
+  });
+
+
 });
